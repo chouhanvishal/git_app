cb3d36678e569649939e38d9f2133e308206b77b
amit <amit@webkorps.com>
2022-06-02 17:35:48 +0530

    remove comments

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,20 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[ show edit update destroy ]
 
-  # GET /students or /students.json
   def index
     @students = Student.all
   end
 
-  # GET /students/1 or /students/1.json
   def show
   end
-
-  # GET /students/new
+  
   def new
     @student = Student.new
   end
 
-  # GET /students/1/edit
   def edit
   end
 
-  # POST /students or /students.json
   def create
     @student = Student.new(student_params)
 
@@ -34,7 +29,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /students/1 or /students/1.json
   def update
     respond_to do |format|
       if @student.update(student_params)
@@ -47,7 +41,6 @@ class StudentsController < ApplicationController
     end
   end
 
-  # DELETE /students/1 or /students/1.json
   def destroy
     @student.destroy
 
@@ -58,12 +51,10 @@ class StudentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def student_params
       params.require(:student).permit(:name, :age, :city, :pincode)
     end
